446912e52b8c3075a6877d616efa56bb30beee0b
Teacher-Jorge <79639664+Teacher-Jorge@users.noreply.github.com>
2022-08-26 16:08:56 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,79 @@
+let btnHeld = document.getElementById('btnHeld')
+let word1 = document.getElementById('word1')
+
+btnHeld.addEventListener('click', function() {
+    word1.innerText = 'was held up by / because of / due to / owing to / on account of / as a result of unforeseen '
+})
+
+//02
+let btnAnything = document.getElementById('btnAnything')
+let word2 = document.getElementById('word2')
+
+btnAnything.addEventListener('click', function() {
+    word2.innerText = "wasn't anything I could do except / other than / but / apart from"
+})
+
+//03
+let btnWithout = document.getElementById('btnWithout')
+let word3 = document.getElementById('word3')
+
+btnWithout.addEventListener('click', function() {
+    word3.innerText = 'without her brother advising / having advised her when / while she was'
+})
+
+//04
+let btnSubject = document.getElementById('btnSubject')
+let word4 = document.getElementById('word4')
+
+btnSubject.addEventListener('click', function() {
+    word4.innerText = "subject to the councils's agreement / subject to the agreement of the council"
+})
+
+//05
+let btnWhich = document.getElementById('btnWhich')
+let word5 = document.getElementById('word5')
+
+btnWhich.addEventListener('click', function() {
+    word5.innerText = "was her trainer's foreign accent which made / was making"
+})
+
+//06
+let btnGet = document.getElementById('btnGet')
+let word6 = document.getElementById('word6')
+
+btnGet.addEventListener('click', function() {
+    word6.innerText = "does Tim get the / any / an opportunity / opportunities to play / of playing"
+})
+
+//07
+let btnDrew = document.getElementById('btnDrew')
+let word7 = document.getElementById('word7')
+
+btnDrew.addEventListener('click', function() {
+    word7.innerText = "drew our attention to"
+})
+
+//08
+let btnRemain = document.getElementById('btnRemain')
+let word8 = document.getElementById('word8')
+
+btnRemain.addEventListener('click', function() {
+    word8.innerText = "are asked to / requested to remain / seated / in their seats"
+})
+
+//09
+let btnIs = document.getElementById('btnIs')
+let word9 = document.getElementById('word9')
+
+btnIs.addEventListener('click', function() {
+    word9.innerText = "no circumstances is this door / the door (ever) to be"
+})
+
+//10
+let btnRight = document.getElementById('btnRight')
+let word10 = document.getElementById('word10')
+
+btnRight.addEventListener('click', function() {
+    word10.innerText = "it clear (that) you have the / every / a right to"
+})
+
